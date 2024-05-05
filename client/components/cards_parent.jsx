ef821fcd2e8ddfbed356e66095a8567b527ff45e
burjoy <luqman.hadi85@gmail.com>
2024-05-05 19:39:59 +0700

    changed fetch direct from database

--- client/components/cards_parent.jsx
+++ client/components/cards_parent.jsx
@@ -1,22 +1,43 @@
 import './styles.css'
-function Cards(){
-    const opt = [1,2,3,4,5,6,7,8,9,4,2];
+function Cards({nama, pasangan}){
+    // const opt = [1,2,3,4,5,6,7,8,9,4,2];
+    // let suami;
+    // let istri;
+    let menikah;
+    // console.log(orang_tua?.pasangan);
+    // if(gender == "laki-laki"){
+    //   suami = nama;
+    //   istri = pasangan;
+    // }
+    // else if(gender == "perempuan"){
+    //   istri = nama;
+    //   suami = pasangan;
+    // }
+    console.log(pasangan);
+    if(pasangan == "" || pasangan == undefined){
+      menikah = 0;
+    }
+    else{
+      menikah = 1;
+    }
     return(
     <>
     <div className="flex justify-center space-x-8">
       <div className="person hover:cursor-pointer p-5 rounded-md">
         <img src="https://placehold.co/100x100" alt="Father" />
         {/* <div className="connection"></div> */}
-        <div className="person-name text-center">Father</div>
+        <div className="person-name text-center">{nama}</div>
       </div>
 
       {/* tar tambahin option kalo si orang udah nikah apa belom, kyk di project 6 */}
 
-      <div className="person p-5 rounded-md">
+      {menikah == 1 && 
+        <div className="person p-5 rounded-md">
         <img src="https://placehold.co/100x100" alt="Mother" />
         {/* <div className="connection"></div> */}
-        <div className="person-name text-center">Mother</div>
+        <div className="person-name text-center">{pasangan}</div>
       </div>
+      }
     </div>
 
     {/* {opt.map((dcs, index) => (
@@ -28,5 +49,5 @@ function Cards(){
     </>
     )
 }
-
+Cards.propTypes
 export {Cards}
