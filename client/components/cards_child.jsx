10b9eb5809fa1fdc816d9d90dbdf6440e7a55f56
burjoy <luqman.hadi85@gmail.com>
2024-05-05 19:39:26 +0700

    added route to next descendant

--- client/components/cards_child.jsx
+++ client/components/cards_child.jsx
@@ -1,10 +1,18 @@
 import "./styles.css";
 import { Adder } from "./adder_form";
 import { useState, useEffect } from "react";
+import { AddAnak } from "./add_anak";
+// import { useGlobalContext } from "../context/utils";
+import get_anggota_keluarga from "../../server/apis/get_anggota_keluarga";
+import get_anggota_keluarga_spesifik from "../../server/apis/get_anggota_keluarga_spesifik";
+import { Link } from "react-router-dom";
 
-function Card_child() {
-  const opt = [1,2,3,4,5,6,7,8,9,4,2];
+function Card_child({anaks, generation, pilihKeluarga, orang_tua}) {
+  // const opt = [1,2,3,4,5,6,7,8,9,4,2];
+  // const opt = [];
+  const gens = generation + 1;
   const [isAdderCall, setAdderCalled] = useState(null);
+  const [add_anak, set_add_anak] = useState(null);
 
   useEffect(() => {
     // setAdderCalled(isAdderCall);
@@ -12,25 +20,43 @@ function Card_child() {
     if(isAdderCall == false){
       setAdderCalled(true);
     }
-  }, [isAdderCall]);
+    if(add_anak == false){
+      set_add_anak(true);
+    }
+  }, [isAdderCall, add_anak]);
 
   const handleClick = () => {
     console.log("ba");
     setAdderCalled(() => (!isAdderCall));
   }
+
+  const handleAddAnak = () => {
+    console.log("ba");
+    set_add_anak(() => (!add_anak));
+  }
+  console.log(anaks);
   return (
     <>
       <div className="grid grid-cols-2 gap-4 mt-9 justify-items-center md:grid-cols-5">
-        {opt.map((urutan, index) => (
+        {anaks?.map((urutan, index) => (
           <>
-          <div className="person hover:cursor-pointer p-5 rounded-md" key={index}>
+          <div className="person hover:cursor-pointer p-5 rounded-md" key={urutan._id}>
             <img src="https://placehold.co/100x100" alt="Daughter"/>
             {/* <!-- <div className="connection"></div> --> */}
             <div className="person-name text-center">{urutan}
             </div>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-3 text-sm">
-              Open Ancestry
-            </button>
+            <Link to={`/${gens}/${urutan}`} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-3 text-sm">
+              Buka Daftar Keluarga
+            </Link>
+            {/* <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-3 text-sm" onClick={async() => 
+              {
+                const family = await get_anggota_keluarga_spesifik(gens, urutan);
+                console.log(family[0]?.anak);
+                pilihKeluarga(family[0]);
+              }
+            }>
+              Buka Daftar Keluarga
+            </button> */}
 
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-3 text-sm"  onClick={handleClick}>
               Edit Profile
@@ -38,10 +64,20 @@ function Card_child() {
           </div>
         </>
         ))}
+          <div className="person hover:cursor-pointer p-7 rounded-md">
+            {/* <img src="https://placehold.co/100x100" alt="Daughter"/> */}
+            {/* <!-- <div className="connection"></div> --> */}
+            <div className="person-name text-center">Tambah Anak
+            </div>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-3 text-sm" onClick={handleAddAnak}>
+              Add Anak
+            </button>
+          </div>
       </div>
+      {add_anak && <AddAnak />}
       {isAdderCall && <Adder />}
     </>
   );
 }
-
+Card_child.propTypes
 export { Card_child };
