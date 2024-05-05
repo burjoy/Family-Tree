b5bdc81617d230b06c6a093c7b6534d6c823b079
burjoy <luqman.hadi85@gmail.com>
2024-05-05 19:42:41 +0700

    added api to get descendants data

---
+++ server/apis/get_anggota_keluarga_spesifik.js
@@ -0,0 +1,35 @@
+const get_anggota_keluarga_spesifik = async(gen, nama) => {
+    try {
+        // const {encodedGen, encodedName} = encodeURIComponent(gen, name);
+        // const encodedGen = encodeURI(gen);
+        // const encodedName = encodeURI(name);
+        // const new_name = name?.replace(" ", "%20");
+        const params = {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            // body: JSON.stringify({
+            //     generasi: gen,
+            //     nama: name
+            // })
+        }
+        const response = await fetch(`http://localhost:3000/family/${gen}/${nama}`, params);
+        if (!response.ok) {
+            throw new Error('Error fetching family name');
+        }
+        console.log(gen);
+        console.log(encodeURI("Si g"));
+        // console.log(new_name);
+        const hasil = await response.json();
+        console.log(hasil);
+        // else{
+        //     console.log(hasil);
+        // }
+        return hasil;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export default get_anggota_keluarga_spesifik
