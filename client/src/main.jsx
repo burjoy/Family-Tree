cad310b75c1c7fd6cf7b5d4f5d8384a8a1ba89e6
burjoy <luqman.hadi85@gmail.com>
2024-05-05 19:42:07 +0700

    added new component imports

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
+import App from './App.jsx'
 import { Cards } from '../components/cards_parent.jsx'
 import { Card_child } from '../components/cards_child.jsx'
 import { SearchBar } from '../components/search_bar.jsx'
+import { Utama } from '../components/utama.jsx'
+// import { AppRouter } from '../routes/route.jsx'
 // import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Cards />
-    <Card_child />
+    <App />
+    {/* <Cards />
+    <Card_child /> */}
+    {/* <Utama /> */}
+    {/* <AppRouter /> */}
     <SearchBar />
   </React.StrictMode>,
 )
