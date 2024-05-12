ba772481366955a6585aab4048f14b1af1fb63e2
burjoy <luqman.hadi85@gmail.com>
2024-05-12 09:29:07 +0700

    changed to button component

--- client/components/search_bar.jsx
+++ client/components/search_bar.jsx
@@ -1,14 +1,38 @@
+import { useState, useEffect } from "react";
+import { CariPanggilan } from "./cari_panggilan";
+import Loading from "./loadingButton";
+
 function SearchBar(){
+    const [called, isCalled] = useState(null); //ini nanti diganti jadi [called, setCalled] = useState('');
+    const [loading, setLoading] = useState(false);
+    const timeOut = setTimeout(() => {setLoading(false)}, 10000);
+    const handleLoading = () => {
+        setLoading(true);
+        timeOut;
+        // setLoading(false);
+    }
+    useEffect(() => {
+        // setAdderCalled(isAdderCall);
+        console.log(called);
+        if(called == false){
+          isCalled(true);
+        }
+      }, [called]);
     const handleSubmit = (event) => {
         console.log("Hello");
         event.preventDefault(); //buat testing doang saat ini
+        isCalled(() => (!called));
     }
 
     return(
         <>
-            <form action="" onSubmit={handleSubmit} className="text-center mt-3">
-                <input type="text" className="border border-gray-300 rounded p-2 text-center"/>
-            </form>
+        <div className="text-center">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-3 text-sm"  onClick={handleLoading}>
+              Cari Panggilan
+            </button>
+        </div>
+        {loading && <Loading />}
+        {called && <CariPanggilan />}
         </>
     )
 }
