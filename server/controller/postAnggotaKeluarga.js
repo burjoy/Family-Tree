9cdb0a6f87be5805d0b6657662744a0fe92b6a64
burjoy <luqman.hadi85@gmail.com>
2024-07-08 19:25:37 +0700

    added new controller to add new family member

---
+++ server/controller/postAnggotaKeluarga.js
@@ -0,0 +1,19 @@
+const {data_personal_keluarga} = require('../model/data');
+
+const post_anggota_keluarga = async(req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+const patch_anggota_keluarga = async(req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+export {post_anggota_keluarga, patch_anggota_keluarga}
