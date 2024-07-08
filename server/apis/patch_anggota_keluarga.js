17ba5182631c1e28c044c8fb427aeb1a3d42e5c9
burjoy <luqman.hadi85@gmail.com>
2024-07-08 19:24:49 +0700

    added api to post and add new family member

---
+++ server/apis/patch_anggota_keluarga.js
@@ -0,0 +1,9 @@
+async function patch_anggota_keluarga(orangTua, anak){
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+export default patch_anggota_keluarga;
---
+++ server/apis/post_anggota_keluarga.js
@@ -0,0 +1,9 @@
+async function post_anggota_keluarga(orangTua, anak, generasi){
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+export default post_anggota_keluarga;
