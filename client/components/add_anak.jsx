db7f4371af487deeb36622e018906f087c829787
burjoy <luqman.hadi85@gmail.com>
2024-07-08 19:26:13 +0700

    added call api to add new family member

--- client/components/add_anak.jsx
+++ client/components/add_anak.jsx
@@ -1,8 +1,40 @@
 import { useState, useRef } from "react";
+import post_anggota_keluarga from "../../server/apis/post_anggota_keluarga";
+import patch_anggota_keluarga from "../../server/apis/patch_anggota_keluarga";
 
-function AddAnak(){
+function AddAnak({generasi, parent}){
     const formRef = useRef(null);
     const [click, isClick] = useState(true);
+    const [nama_anak, set_nama_anak] = useState("");
+    const [urutan_anak, set_urutan_anak] = useState();
+
+    const handleNamaAnak = (name) => {
+        set_nama_anak(name);
+        console.log(nama_anak);
+    }
+
+    const handleUrutanAnak = (urutan) => {
+        set_urutan_anak(urutan);
+    }
+
+    const submitHasilKeluarga = async() => {
+        try {
+            const response = await post_anggota_keluarga;
+            console.log(response);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const patchKeluarga = async() => {
+        try {
+            const response = await patch_anggota_keluarga;
+            console.log(response);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     const handleClick = (e) => {
         console.log(formRef.current)
         if (formRef.current && formRef.current.contains(e.target)) {
@@ -21,17 +53,14 @@ function AddAnak(){
         <>
             {click && (
              <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50" onClick={handleClick}>
-                    <div ref={formRef} className="bg-white rounded-md p-4">
+                    <div ref={formRef} className="bg-white rounded-md p-4 text-center">
                         <h2 className="text-center">Form penambahan data anak</h2>
                         <form action="">
                             <p>Nama Anak</p>
-                            <input type="text" className="border rounded-lg text-center focus:border-red-600"/>
-
-                            <p>Nama Panggilan</p>
-                            <input type="text" className="border rounded-lg text-center focus:border-red-600" />
+                            <input type="text" className="border rounded-lg text-center focus:border-red-600" onChange={e => handleNamaAnak(e.target.value)}/>
 
                             <p>Urutan Anak</p>
-                            <input type="text" className="border rounded-lg text-center focus:border-red-600" />
+                            <input type="text" className="border rounded-lg text-center focus:border-red-600" onChange={e => handleUrutanAnak(parseInt(e.target.value))}/>
                         </form>
                         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-5">
                             Submit Hasil
@@ -43,4 +72,5 @@ function AddAnak(){
     );
 }
 
+AddAnak.propTypes;
 export {AddAnak};
