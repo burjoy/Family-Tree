a3df6d0dc80b05d57513d4b577511f3323079b5f
burjoy <luqman.hadi85@gmail.com>
2024-07-08 19:25:21 +0700

    added new endpoint to add new family member

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const getFamily = require('./controller/getFamilyGen');
+const { post_anggota_keluarga, patch_anggota_keluarga } = require('./controller/postAnggotaKeluarga');
 
 const port = 3000;
 
@@ -22,6 +23,8 @@ mongoose.connect(process.env.LINK_ATLAS).
 
 app.post('/family', getFamily.getKeluarga);
 app.post('/family/:gen/:names', getFamily.getKeluargaSpesifik);
+app.post('/family/postFamily', post_anggota_keluarga);
+app.post('/family/patchFamily', patch_anggota_keluarga);
 
 app.listen(port, () => {
     console.log(`Server berjalan di ${port}`);
