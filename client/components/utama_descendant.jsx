410ee632ff06c66bd2687b3b5790fe0189be30d0
burjoy <luqman.hadi85@gmail.com>
2024-07-13 20:17:02 +0700

    added add anak functionality in descendant component

--- client/components/utama_descendant.jsx
+++ client/components/utama_descendant.jsx
@@ -6,6 +6,8 @@ import { useEffect, useState } from "react";
 // import get_anggota_keluarga from "../../server/apis/get_anggota_keluarga";
 import { useParams } from "react-router-dom";
 import get_anggota_keluarga_spesifik from "../../server/apis/get_anggota_keluarga_spesifik";
+import { AddAnak } from "./add_anak";
+import { Adder } from "./adder_form";
 
 function Utama_descendant(){
     // const [loadKeluarga, isLoadKeluarga] = useState();
@@ -19,6 +21,9 @@ function Utama_descendant(){
     //     }
     // }
     const [loadKeluarga, setLoadKeluarga] = useState();
+    const [isAdderCall, setAdderCalled] = useState(null);
+    const [add_anak, set_add_anak] = useState(null);
+    const [addAnggotaBaru, isAnggotaBaruAdded] = useState();
     const pilihKeluarga = (loadKeluarga) => {
         setLoadKeluarga(loadKeluarga);
     }
@@ -48,11 +53,35 @@ function Utama_descendant(){
         // if(hasil.suami != ""){
         //     console.log(hasil.suami);
         // }
-    }, []);
+        if(isAdderCall == false){
+            setAdderCalled(true);
+        }
+    }, [addAnggotaBaru]);
+    useEffect(() => {
+        if(add_anak == false){
+            set_add_anak(true);
+        }
+    }, [add_anak]);
+    const handleAddAnak = () => {
+        set_add_anak(!add_anak);
+        console.log(add_anak);
+    }
     return(
         <>
             <Cards nama={loadKeluarga?.nama} pasangan={loadKeluarga?.pasangan}/>
             <Card_child anaks={loadKeluarga?.anak} generation={loadKeluarga?.generasi} pilihKeluarga={pilihKeluarga} orang_tua={loadKeluarga?.nama}/>
+            <div className="person hover:cursor-pointer p-7 rounded-md mt-10 w-[30%] mx-auto">
+                {/* <img src="https://placehold.co/100x100" alt="Daughter"/> */}
+                {/* <!-- <div className="connection"></div> --> */}
+                <div className="person-name text-center">Tambah Anak
+                </div>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-3 text-sm" onClick={handleAddAnak}>
+                Add Anak
+                </button>
+            </div>
+            {/* <p>{counter}</p><button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-5" onClick={() => incCounter(counter + 1)}></button> */}
+            {add_anak && <AddAnak generasi={loadKeluarga?.generasi + 1} parent={loadKeluarga?.nama} anggota_baru={isAnggotaBaruAdded}/>}
+            {isAdderCall && <Adder />}
         </>
     )
 }
