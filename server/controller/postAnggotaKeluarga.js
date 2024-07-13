40b813d011b7e6167021a59d1afa26d2ef0662ae
burjoy <luqman.hadi85@gmail.com>
2024-07-13 20:11:48 +0700

    added add new family member control action

--- server/controller/postAnggotaKeluarga.js
+++ server/controller/postAnggotaKeluarga.js
@@ -13,6 +13,15 @@ const patch_anggota_keluarga = async(req, res) => {
         const {nama_anak, nama_orang_tua} = req.body;
         console.log(nama_orang_tua);
         console.log(nama_anak);
+        // const existing_document = await data_personal_keluarga.findOne({"nama": nama_orang_tua});
+        // console.log(existing_document.anak);
+        const updatedDoc = await data_personal_keluarga.findOneAndUpdate({"nama": nama_orang_tua}, {$push: {
+            "anak": nama_anak
+        }});
+        console.log(updatedDoc);
+        const doc = await data_personal_keluarga.findOne({"nama": nama_orang_tua});
+        console.log(doc);
+        res.json(doc);
     } catch (error) {
         console.log(error);
     }
