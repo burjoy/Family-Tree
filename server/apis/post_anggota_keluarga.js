3910dce8a2e4326421ca4e023f9d2cc26e1181f6
burjoy <luqman.hadi85@gmail.com>
2024-08-06 20:40:50 +0700

    added a new function to post to database

--- server/apis/post_anggota_keluarga.js
+++ server/apis/post_anggota_keluarga.js
@@ -1,8 +1,26 @@
-async function post_anggota_keluarga(orangTua, anak, generasi){
+async function post_anggota_keluarga(orangTua, anak, generasi, status_menikah, nama_pasangan, jenis_kelamin, tanggal_lahir){
     try {
-        
+        const pesan_request = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                nama_anak: anak,
+                nama_orang_tua: orangTua,
+                generation: generasi,
+                pasangan: nama_pasangan,
+                kelamin: jenis_kelamin,
+                banyak_anak: [],
+                date_of_birth: tanggal_lahir,
+                menikah: status_menikah
+            })
+        }
+        const response = await fetch('http://localhost:3000/family/postFamily', pesan_request);
+        const hasil = response.json();
+        console.log(hasil);
     } catch (error) {
-        
+        console.log(error);
     }
 }
 
