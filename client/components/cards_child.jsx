f2edbdc9698bb6602b8633b5726bea7727a5d892
burjoy <luqman.hadi85@gmail.com>
2024-08-06 20:38:36 +0700

    added mechanism to call data_diri component

--- client/components/cards_child.jsx
+++ client/components/cards_child.jsx
@@ -6,30 +6,36 @@ import { AddAnak } from "./add_anak";
 import get_anggota_keluarga from "../../server/apis/get_anggota_keluarga";
 import get_anggota_keluarga_spesifik from "../../server/apis/get_anggota_keluarga_spesifik";
 import { Link } from "react-router-dom";
+import { Data_diri } from "./data_diri";
 
 function Card_child({anaks, generation, pilihKeluarga, orang_tua}) {
   // const opt = [1,2,3,4,5,6,7,8,9,4,2];
   // const opt = [];
   const gens = generation + 1;
+  console.log(`Generasi anak saat ini: ${gens}, generasi orang tua: ${generation}`);
   const [isAdderCall, setAdderCalled] = useState(null);
+  const [status, setStatus] = useState(null);
+  const [selectedUrutan, setSelectedUrutan] = useState(null);
   // const [add_anak, set_add_anak] = useState(null);
 
   useEffect(() => {
     // setAdderCalled(isAdderCall);
-    console.log(isAdderCall);
-    if(isAdderCall == false){
-      setAdderCalled(true);
+    console.log(status);
+    if(status == false){
+      setStatus(true);
     }
     // if(add_anak == false){
     //   set_add_anak(true);
     // }
-  }, [isAdderCall]);
+  }, [status]);
 
-  const handleClick = () => {
+  const handleClick = (nama_keluarga) => {
     console.log("ba");
-    setAdderCalled(() => (!isAdderCall));
+    // setAdderCalled(() => (!isAdderCall));
+    // setSelectedUrutan(nama_keluarga);
+    setStatus(() => (!status));
+    setSelectedUrutan(nama_keluarga);
   }
-
   // const handleAddAnak = () => {
   //   console.log("ba");
   //   set_add_anak(() => (!add_anak));
@@ -41,11 +47,11 @@ function Card_child({anaks, generation, pilihKeluarga, orang_tua}) {
         {anaks?.map((urutan, index) => (
           <>
           <div className="person hover:cursor-pointer p-5 rounded-md" key={index}>
-            <img src="https://placehold.co/100x100" alt="Daughter"/>
+            <img src="https://placehold.co/100x100" alt="Daughter" className="max-md:w-[75px] max-md:h-[75px]"/>
             {/* <!-- <div className="connection"></div> --> */}
             <div className="person-name text-center">{urutan}
             </div>
-            <Link to={`/${gens}/${urutan}`} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-3 text-sm">
+            <Link to={`/${gens}/${urutan}`} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-3 text-sm max-md:text-xs max-md:py-2 max-md:px-2">
               Buka Daftar Keluarga
             </Link>
             {/* <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-3 text-sm" onClick={async() => 
@@ -62,8 +68,8 @@ function Card_child({anaks, generation, pilihKeluarga, orang_tua}) {
               Edit Profile
             </button> */}
 
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-3 text-sm"  onClick={handleClick}>
-              Ganti Nama
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-3 text-sm max-md:text-xs max-md:py-2 max-md:px-2"  onClick={() => handleClick(urutan)}>
+              Ganti Status
             </button>
           </div>
         </>
@@ -78,6 +84,7 @@ function Card_child({anaks, generation, pilihKeluarga, orang_tua}) {
             {/* </button> */}
           {/* </div> */}
       </div>
+      {status === true && selectedUrutan !== null && (<Data_diri generasi={gens} nama={selectedUrutan}/>)}
       {/* {add_anak && <AddAnak generasi={gens} parent={orang_tua}/>}
       {isAdderCall && <Adder />} */}
     </>
