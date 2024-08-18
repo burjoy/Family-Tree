0b8c71b068abe4133c9861ad09251ff0be79b358
burjoy <luqman.hadi85@gmail.com>
2024-08-18 18:47:33 +0700

    added a new controller function to remove troubled family member

--- server/controller/postAnggotaKeluarga.js
+++ server/controller/postAnggotaKeluarga.js
@@ -57,4 +57,23 @@ const patch_anggota_keluarga = async(req, res) => {
     }
 }
 
-module.exports = {post_anggota_keluarga, patch_anggota_keluarga}
+const delete_anggota_keluarga = async(req, res) => {
+    try {
+        const {nama_anak, nama_orang_tua} = req.body;
+        console.log(nama_orang_tua);
+        console.log(nama_anak);
+        // const existing_document = await data_personal_keluarga.findOne({"nama": nama_orang_tua});
+        // console.log(existing_document.anak);
+        const updatedDoc = await data_personal_keluarga.findOneAndUpdate({"nama": nama_orang_tua}, {$pull: {
+            "anak": nama_anak
+        }});
+        console.log(updatedDoc);
+        const doc = await data_personal_keluarga.findOne({"nama": nama_orang_tua});
+        console.log(doc);
+        res.json(doc);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+module.exports = {post_anggota_keluarga, patch_anggota_keluarga, delete_anggota_keluarga}
