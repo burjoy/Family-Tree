9fd668553d7a5392b8328d86fded38b1453f38b0
burjoy <luqman.hadi85@gmail.com>
2024-08-18 18:49:34 +0700

    tweaked mechanism to control whether user wants to add or remove family member

--- server/apis/patch_anggota_keluarga.js
+++ server/apis/patch_anggota_keluarga.js
@@ -1,5 +1,6 @@
-async function patch_anggota_keluarga(orangTua, anak){
+async function patch_anggota_keluarga(orangTua, anak, hapus_dari_kk=false){
     try {
+        var endpoint;
         const pesan_request = {
             method: 'POST',
             headers: {
@@ -10,7 +11,13 @@ async function patch_anggota_keluarga(orangTua, anak){
                 nama_orang_tua: orangTua
             })
         }
-        const response = await fetch('http://localhost:3000/family/patchFamily', pesan_request);
+        if(hapus_dari_kk === true){
+            endpoint = "deleteFamily";
+        }
+        else{
+            endpoint = "patchFamily";
+        }
+        const response = await fetch(`http://localhost:3000/family/${endpoint}`, pesan_request);
         const hasil = response.json();
         console.log(hasil);
         return hasil
