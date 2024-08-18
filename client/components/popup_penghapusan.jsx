7f222ba50178c854037a25f2449e31c16981e7b8
burjoy <luqman.hadi85@gmail.com>
2024-08-18 18:47:11 +0700

    added mechanism to remove troubled family member in database

--- client/components/popup_penghapusan.jsx
+++ client/components/popup_penghapusan.jsx
@@ -1,10 +1,22 @@
-import { useState } from "react"
-function Penghapusan(){
+import { useEffect, useState } from "react"
+import patch_anggota_keluarga from "../../server/apis/patch_anggota_keluarga";
+function Penghapusan({parent, anak}){
     const [oke, isOke] = useState(true);
 
     const handleOke = () => {
         isOke(() => (!oke));
-    } 
+    }
+    useEffect(() => {
+        
+    },[])
+    const handleHapus = async() => {
+        try {
+            const hasil = await patch_anggota_keluarga(parent, anak, true);
+            console.log(hasil);
+        } catch (error) {
+            console.log(error);
+        }
+    }
     return(
         <>
         {oke && (
@@ -18,7 +30,7 @@ function Penghapusan(){
                             <p>Yang ingin dipanggil</p>
                             <input type="text" className="border rounded-lg text-center focus:border-red-600" />
                         </form> */}
-                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-5 mx-5" onClick={handleOke}>
+                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-5 mx-5" onClick={handleHapus}>
                             Ya
                         </button>
                         <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mt-5" onClick={handleOke}>
@@ -31,4 +43,5 @@ function Penghapusan(){
     )
 }
 
+Penghapusan.propTypes
 export {Penghapusan}
